b69f84dcdf7db63b6fdcacb6bfd43cc1c7b4c3d8
Richard Ross <richard@goatrockresearch.org>
2016-09-10 09:18:32 -0700

    Added alt. methods.

--- log-13-05-num-max.js
+++ log-13-05-num-max.js
@@ -3,6 +3,7 @@
 
 //  Given an array with multiple values, write a function that returns the maximum number in the array (e.g. for `[-3,3,5,7]` max is `7`).
 
+//  Method A
 function findMax(arr) {
     var max = arr[0];
     for(var x = 0; x < arr.length; x++){
@@ -11,6 +12,21 @@ function findMax(arr) {
         }
     }
     return max;
+}
+
+//  Method B
+function printMax(arr){
+    if (arr.length == 0){
+        console.log(`Empty array, no max value.`);
+        return;
+    }
+    var max = arr[0];
+    for (var x = 1; x < arr.length; x++){
+        if (arr[x] > max) {
+            max = arr[x];
+        }
+    }
+    console.log(`Max value is:`, max);
 }
 
 //  L|5
