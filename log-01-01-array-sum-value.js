4b8e4364d6f3e393749c02e18f55a22e98c98b80
Richard Ross <richard@goatrockresearch.org>
2016-09-10 09:17:36 -0700

    Initial Commit.

---
+++ log-01-01-array-sum-value.js
@@ -0,0 +1,14 @@
+
+//  Algorithm: Fundamental 01-01
+
+//  Write a function that returns an array with all the numbers from 1 to 255 and the sum.
+
+function printSum1255(){
+    var sum = 0;
+    for (var num = 0; num <= 255; num++){
+        sum += num;
+        console.log(`New number:`, num, `Sum:`, sum);
+    }
+}
+
+//  L|5
---
+++ log-01-02-array-value.js
@@ -0,0 +1,12 @@
+
+//  Algorithm: Fundamental 01-02
+
+//  Write a function that returns all values in a given array.
+
+function arrayValue(arr) {
+    for (var x = 0; x < arr.length; x++) {
+        console.log(`array[`, x,`] is equal to`, arr[x]);
+    }
+}
+
+//  L|5
