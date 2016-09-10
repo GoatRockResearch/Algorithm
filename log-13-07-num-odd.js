eb08c65d42d3c1de8efeab6c416e0f97d3eb54d5
Richard Ross <richard@goatrockresearch.org>
2016-09-10 09:32:08 -0700

    Added alt. f(x).

--- log-13-07-num-odd.js
+++ log-13-07-num-odd.js
@@ -3,6 +3,7 @@
 
 //  Write a function that would return an array of all the odd numbers between 1 to 50 (e.g. `[1,3,5, .... , 47,49]`).
 
+//  Method A
 function oddNumbers() {
     var arr = [];
     for(var x = 1; x < 50; x++){
@@ -11,6 +12,15 @@ function oddNumbers() {
        }
     }
     return arr;
+}
+
+//  Method B
+function oddArray() {
+    var oddArray = [];
+    for (var x = 1; x <= 255; x += 2) {
+        oddArray.push(x);
+    }
+    return oddArray;
 }
 
 //  L|5
--- log-13-08-array-greater.js
+++ log-13-08-array-greater.js
@@ -3,6 +3,7 @@
 
 //  Given value of Y, write a function that takes an array and returns the number of values that are greater than Y (e.g. if `arr = [1, 3, 5, 7]` and `Y = 3`, your function will return `2`).
 
+//  Method A
 function greaterY(arr, Y) {
     var count = 0;
     for(var x = 0; x < arr.length; x++){
@@ -11,6 +12,17 @@ function greaterY(arr, Y) {
         }
     }
     return count;
+}
+
+// Method B
+function numGreater(arr, y){
+    var count = 0;
+    for (var x = 0; x < arr.length; x++) {
+            if (arr[x] > y) {
+            count++;
+        }
+    }
+    console.log("%d values greater than %d", count, y);
 }
 
 //  L|5
