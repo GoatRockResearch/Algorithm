9ed398d8a6abbc7d3db4c17687bd866b2ce9347c
Richard Ross <richard@goatrockresearch.org>
2016-09-14 14:25:27 -0700

    Rename f(x)

--- log-13-05-array-max.js
+++ log-13-05-array-max.js
@@ -4,7 +4,6 @@
 //  Write a f(x) that returns the maximum number in an array;
 //  e.g. for `[-3,3,5,7]` max is `7`;
 
-//  Method A:
 function arrayMax(arr) {
     arr = [-3,3,5,7]
     var max = arr[0];
