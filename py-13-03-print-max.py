02f99acfc10899566088d22d3c0127e8b261bae4
Richard Ross <richard@goatrockresearch.org>
2016-09-15 14:28:31 -0700

    Added f(x)

---
+++ py-13-03-print-max.py
@@ -0,0 +1,13 @@
+
+#   Algorithm: py Basic 13-02
+
+#   Write a f(x) that prints ints 0-255 and sum thereof;
+
+def printMax(numList):
+    max = numList[0]
+    for idx in range(1, len(numList)):
+        if numList[idx] > max:
+            max = numList[idx]
+    return max
+
+#   L|5
